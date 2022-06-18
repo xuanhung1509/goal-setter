79b2222e4562bb4cbe5abcdf285be16da2d0034d
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-06-18 15:39:14 +0700

    Setup user model, routes and controller, authenticate with JWT

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -20,11 +21,13 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
+
   res.status(201).json(goal);
 });
 
-// @desc    Get goals
+// @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
@@ -35,13 +38,27 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found');
   }
 
+  const user = await User.findById(req.user.id);
+
+  // Check if user exists
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // Make sure one cannot modify other's goals
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
   res.status(200).json(updatedGoal);
 });
 
-// @desc    Get goals
+// @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
@@ -50,6 +67,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error('Goal not found');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check if user exists
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // Make sure one cannot modify other's goals
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
   }
 
   await goal.remove();
